4e0085c041ddfa5dcf77861e103d5c568d3e14f8
GiladShmueli <giladsh697@gmail.com>
2021-10-22 23:03:48 +0300

    end of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc; // Module View Controller (View taken from the client) dotnet serving html
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context) //DataContext from API/Data (our implementation)
@@ -21,11 +20,14 @@ namespace API.Controllers
 
 
         [HttpGet] 
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
             return await _context.Users.ToListAsync();
         }
         
+
         //api/users/{id}
+        [Authorize]
         [HttpGet("{Id}")] //"{Id}" - route parameter. looks similar to xaml binding
         public async Task<ActionResult<AppUser>> GetUser(int id){
             return await _context.Users.FindAsync(id);
