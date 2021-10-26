4e61d2698eacc0d72d3a95cc3d8ef986a76393b8
GiladShmueli <giladsh697@gmail.com>
2021-10-26 03:15:27 +0300

    end of section 6

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+// basically doing localhost:4200/{path} similarly to express
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]}, //canActivate prop is redundant, but remains for the sake of learning
+      {path: 'members/:id', component: MemberDetailComponent},
+      {path: 'lists', component: ListsComponent},
+      {path: 'messages', component: MessagesComponent},
+    ]
+  },
+  
+  {path: '**', component: HomeComponent, pathMatch: 'full'}, //wild card! the default of a switch case sorta. pathMatch = prefix or full
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
